71227c16c7485f0aa9e84ab9185512a2c98dd3f4
Mahmoud Samir <mahmoudbadr9199@gmail.com>
2022-06-29 10:30:42 +0200

    init commit

---
+++ src/agent.spec.ts
@@ -0,0 +1,126 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+  ethers,
+} from "forta-agent";
+import { 
+  MockEthersProvider,
+  //encodeParameter, 
+  MockEthersSigner, 
+  createAddress,
+} from "forta-agent-tools/lib/tests";
+import { utils, Contract } from "ethers";
+import agent, {
+  SWAP_EVENT,
+  UNISWAP_ROUTER_ADDRESS,
+} from "./agent";
+
+import { UNISWAP_V3_POOL_ADDRESS } from "../RouterABI";
+import { NON_UNISWAP_V3_POOL_ADDRESS } from "../RouterABI";
+
+describe("uniswap bot", () => {
+  let handleTransaction: HandleTransaction;
+  const mockTxEvent = createTransactionEvent({} as any);
+
+  beforeAll(async () => {
+      handleTransaction = agent.handleTransaction;
+  });
+
+  describe("handleTransaction", () => {
+    /*it("returns empty findings if there are no swapping events", async () => {
+      mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        SWAP_EVENT,
+      );
+    });*/
+
+    it("returns a finding if there is a swapping event from uniswap", async () => {
+      // sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick
+      console.log("START");
+      const mockSwappingEvent = {
+        args: {
+          sender: "0xabc",
+          recipient: "0xdef",
+          amount0: "10",
+          amount1: "11",
+          sqrtPriceX96: "12",
+          liquidity: "13",
+          tick:"14",
+        },
+        address: UNISWAP_V3_POOL_ADDRESS
+      };
+      mockTxEvent.filterLog = jest
+        .fn()
+        .mockReturnValue([mockSwappingEvent]);
+
+      const findings = await handleTransaction(mockTxEvent);
+      console.log(findings);
+
+      const sender = mockSwappingEvent.args.sender;
+      const recipient = mockSwappingEvent.args.recipient;
+      const amount0 = mockSwappingEvent.args.amount0;
+      const amount1 = mockSwappingEvent.args.amount1;
+      const sqrtPriceX96 = mockSwappingEvent.args.sqrtPriceX96;
+      const liquidity = mockSwappingEvent.args.liquidity;
+      const tick = mockSwappingEvent.args.tick;
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "UNISWAP V3 swap",
+          description: `Swap event`,
+          alertId: "FORTA-1",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            sender,
+            recipient,
+            amount0,
+            amount1,
+            sqrtPriceX96,
+            liquidity,
+            tick
+          },
+          addresses: []
+        })
+      ]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        SWAP_EVENT,
+      );
+      console.log("FINISH");
+    });
+
+    /*it("returns no findings if there's a swapping event not from uniswap", async() => {
+      const mockSwappingEvent = {
+        args: {
+          sender: "0xabc",
+          recipient: "0xdef",
+          amount0: "10",
+          amount1: "11",
+          sqrtPriceX96: "12",
+          liquidity: "13",
+          tick:"14",
+        },
+        address: NON_UNISWAP_V3_POOL_ADDRESS
+      };
+      mockTxEvent.filterLog = jest
+        .fn()
+        .mockReturnValue([mockSwappingEvent]);
+
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        SWAP_EVENT,
+      );
+    })*/
+  });
+});
---
+++ src/agent.ts
@@ -0,0 +1,93 @@
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  getEthersProvider,
+} from "forta-agent";
+
+import {POOL_ABI, FACTORY_ABI, FACTORY_ADDRESS} from '../RouterABI';
+import { ethers } from "ethers";
+/*
+const Web3 = require("web3");
+const infura_URL = "https://mainnet.infura.io/v3/88c8b23728d749a3bccb188917e9ec27";
+const web3 = new Web3(new Web3.providers.HttpProvider(infura_URL));
+*/
+export const SWAP_EVENT =
+  "event Swap (address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
+export const UNISWAP_ROUTER_ADDRESS = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+//export const TETHER_DECIMALS = 6;
+let findingsCount = 0;
+const handleTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
+
+  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
+  if (findingsCount >= 1) return findings;
+
+  const uniswapSwapEvents = txEvent.filterLog(
+    SWAP_EVENT
+  );
+
+  const isUniswapAddress = async (currPool: string) => {
+    // compare factory contract address with our constant
+    const provider = getEthersProvider();
+    // Query the pool contract for tokens and fee
+    const poolContract = new ethers.Contract(currPool, POOL_ABI, provider);
+    const factory = await poolContract.factory();
+
+    if(factory.toLowerCase() == FACTORY_ADDRESS.toLowerCase()){
+      return true;
+    }
+
+    return false;
+  }
+
+  uniswapSwapEvents.forEach(async (swapEvent) => {
+    // extract transfer event arguments
+    const { sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick } = swapEvent.args;
+    // shift decimals of transfer value
+    console.log(swapEvent);
+    //const isUniPool = await isUniswapAddress(swapEvent.address);
+    if(await isUniswapAddress(swapEvent.address)) {
+      //const swapEvents = txEvent.filterLog(SWAP_EVENT, swapEvent.address);
+      console.log("inside");
+      findings.push(
+        Finding.fromObject({
+          name: "UNISWAP V3 swap",
+          description: `Swap event`,
+          alertId: "FORTA-1",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            sender,
+            recipient,
+            amount0,
+            amount1,
+            sqrtPriceX96,
+            liquidity,
+            tick
+          },
+        })
+      )
+      findingsCount++;
+    }
+  });
+
+  return findings;
+};
+
+// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some block condition
+//   return findings;
+// }
+
+export default {
+  handleTransaction,
+  // handleBlock
+};
